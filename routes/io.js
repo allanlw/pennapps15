3107c2004988d4d8301fc02154aae543447aadd5
Allan Wirth <allan@allanwirth.com>
2015-01-17 01:54:49 -0500

    Add basic queue and dispatching

--- routes/io.js
+++ routes/io.js
@@ -1,12 +1,44 @@
 var uuid = require('node-uuid');
 
+var queue = require('../lib/queue');
+
+var available_workers = [];
+
 function serve_task(req) {
-  req.io.emit("do-task", {
+  if (queue.size() === 0) {
+    available_workers.push(req.io);
+    console.log(available_workers.length + " free workers");
+  } else {
+    req.io.emit("do-task", queue.dequeue());
+  }
+}
+
+function addTest(io) {
+  io.emit("do-task", {
     script: "var x = 0; for(i = 0, x = 0; i < input.max; i++) { x+= i*i; } return x;",
     input: JSON.stringify({max: 100000000 * Math.random()}),
     uuid: uuid.v4(),
   });
 }
+
+function testRandomlyForever() {
+  if (available_workers.length > 0) {
+    addTest(available_workers.shift());
+  }
+  // one every minuteish
+  setTimeout(testRandomlyForever, Math.random() * 1000 * 15);
+}
+
+
+testRandomlyForever();
+
+queue.on("enqueue", function(x) {
+  if (available_workers.length > 0) {
+    available_workers.shift().emit("do-task", queue.dequeue());
+  } else {
+    console.log(queue.size() + " queued jobs");
+  }
+});
 
 function add_io_routes(app) {
   app.io.route('ready', function(req) {
