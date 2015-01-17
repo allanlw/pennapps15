9f1198badb97cc3d577e170f62eda4e37f2914ec
Allan Wirth <allan@allanwirth.com>
2015-01-16 20:47:15 -0500

    Fix issues with express(.io)

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var ioroutes = require('./routes/io');
 
 var app = express();
+app.http().io();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +26,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+// Add all the socket.io routes to the app
+ioroutes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
