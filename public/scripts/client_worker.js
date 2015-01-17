302d89b86d332c76a55ffa5b19096dda013ec495
Allan Wirth <allan@allanwirth.com>
2015-01-16 23:45:53 -0500

    Add command line client

--- public/scripts/client_worker.js
+++ public/scripts/client_worker.js
@@ -1,4 +1,5 @@
 importScripts('/socket.io/socket.io.js')
+importScripts('/scripts/client_worker_inner.js');
 
 function postJSON(event, data) {
   postMessage(JSON.stringify({'event': event, 'data': data}));
@@ -6,23 +7,4 @@ function postJSON(event, data) {
 
 io = io.connect();
 
-io.emit('ready');
-
-postJSON('ready');
-
-io.on('do-task', function(data) {
-  postJSON('do-task', data);
-
-  var input = JSON.parse(data.input);
-
-  var result = null, exc = null;
-  try {
-    result = (new Function("input", data.script))(input);
-  } catch(e) {
-    exc = e;
-  }
-
-  var ro = {"uuid": data.uuid, "result": result, "exception": exc};
-  postJSON('task-done', ro);
-  io.emit('task-done', ro);
-});
+initClient(io, postJSON);
---
+++ public/scripts/client_worker_inner.js
@@ -0,0 +1,28 @@
+// Inner script that is shared between node version of
+// client and the webworker version of the client
+function initClient(io, postJSON) {
+  io.emit('ready');
+
+  postJSON('ready');
+
+  io.on('do-task', function(data) {
+    postJSON('do-task', data);
+
+    var input = JSON.parse(data.input);
+
+    var result = null, exc = null;
+    try {
+      result = (new Function("input", data.script))(input);
+    } catch(e) {
+      exc = e;
+    }
+
+    var ro = {"uuid": data.uuid, "result": result, "exception": exc};
+    postJSON('task-done', ro);
+    io.emit('task-done', ro);
+  });
+}
+
+if (typeof(module) !== "undefined") {
+  module.exports = initClient;
+}
