53141a58e28b110266709e617bfa5a15261b7d1c
Winston Chen <winstonc@bu.edu>
2015-01-17 23:15:59 -0500

    GUI for any file submission

--- util/MasterServer/routes/index.js
+++ util/MasterServer/routes/index.js
@@ -6,23 +6,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
-    var data = {
-    script: "var x = 0; for(i = 0, x = 0; i < input.max; i++) { x+= i*i; } return x;",
-    input: JSON.stringify({max: 100000000 * Math.random()}),
-    url: 'http://localhost:4500/getMaster'
+router.get('/', function(req, res){
+  res.render('index', { title: 'home' });
+});
+
+router.post('/', function(req, res){
+  res.render('index', { title: 'Post succeeded' });
+  //var fakeScript = "var x = 0; for(i = 0, x = 0; i < input.max; i++) { x+= i*i; } return x;";
+  var data = {
+  script: req.body.script,
+  input: JSON.stringify({max: 100000000 * Math.random()}),
+  url: 'http://localhost:4500/getMaster'
   };
     request.post(
     'http://localhost:3000/postMaster',
     { json: data },
     function (error, response, body) {
         console.log(body);
-	});
+  });
 });
 
 router.post('/getMaster', function(req, res) {
-  //res.render('test', { title: 'Client' });
   console.log(req.body);
 });
 
